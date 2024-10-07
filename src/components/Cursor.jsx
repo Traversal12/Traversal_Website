61bf5b6188d590cbe9bddc22cdbe1b4be951603f
AyushiMaithani <ayushimaithani04@gmail.com>
2024-10-07 09:53:18 +0530

    fix cursor visibility issue

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -24,7 +24,8 @@ function Cursor({cursorVisible}) {
     const variants = { left: position.x, top: position.y };
   return (
     <motion.div
-    className={`fixed top-0 left-0 bg-[#00a6ff] rounded-full h-4 w-4 z-50 opacity-${cursorVisible} pointer-events-none`}
+    className='fixed top-0 left-0 bg-[#00a6ff] rounded-full h-4 w-4 z-50 pointer-events-none'
+    style={{ opacity:cursorVisible  }}
     variants={variants}
     animate={variants} 
     transition={{ type: 'spring', stiffness: 300, damping: 20 }}
